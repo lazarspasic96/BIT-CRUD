24d4faac37ac08e100d7c3a25fccee51b7de031e
BIT Student <classroom@bgit.rs>
2019-12-06 15:52:29 +0100

    mdbreact is finaly added

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import FormPage from '../siginUp/FormPage';
 
 class Main extends React.Component {
 
@@ -7,15 +8,22 @@ class Main extends React.Component {
 
 
     render() {
-        return <>
+        return <main>
+
 
             This is Main
+
+                   <FormPage />
             <Switch>
                 <Route exact path='/' />
                 <Route exact path='/about' />
 
             </Switch>
-        </>
+
+
+        </main>
+
+
     }
 }
 
