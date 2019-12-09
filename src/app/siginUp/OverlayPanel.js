5c809def24f2662885605e5ac051aa7ca48a67d7
BIT Student <classroom@bgit.rs>
2019-12-09 16:06:53 +0100

    Seperate Sigin form on componenet

---
+++ src/app/siginUp/OverlayPanel.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+class OverlayPanel extends React.Component {
+
+
+    render() {
+        return <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost btnImportant" id="signIn" onClick={this.props.handleClick}>Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost btnImportant" id="signUp" onClick={this.props.handleClick}>Sign Up</button>
+                </div>
+            </div>
+        </div>
+
+
+    }
+}
+
+export default OverlayPanel
