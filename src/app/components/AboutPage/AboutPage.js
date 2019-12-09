bbdb6de5b436044c963ec8289ac6a69c7a74dad0
BIT Student <classroom@bgit.rs>
2019-12-09 17:04:25 +0100

    About Page is done

--- src/app/components/AboutPage/AboutPage.js
+++ src/app/components/AboutPage/AboutPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './about.css';
+
 
 class AboutPage extends React.Component  {
     render () {
         return (
-            <div>
+            <div id ="about-container">
                 <h2> ABOUT </h2>
                 <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique quaerat reiciendis autem dolore quisquam iure inventore, 
+                <p id="about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio similique quaerat reiciendis autem dolore quisquam iure inventore, 
                     nobis quasi aperiam facilis, optio dicta. Nam magnam odio quo explicabo incidunt blanditiis animi ex corrupti facilis nobis dolor
                      in possimus officiis soluta at a, aliquid quibusdam totam! Laudantium porro sint amet quia?Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Odio similique quaerat reiciendis autem dolore quisquam iure inventore, nobis quasi aperiam facilis, optio dicta. 
