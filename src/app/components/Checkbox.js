65800cc6455fb519c6ce649534e55cda59321a0e
BIT Student <classroom@bgit.rs>
2019-12-09 11:21:32 +0100

    Just added Checkbox and RadiBox to our project

---
+++ src/app/components/Checkbox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+class Checkbox extends React.Component {
+
+    render() {
+        return <>
+            <label for={this.props.for}>
+                {this.props.children}  <input type="checkbox" name={this.props.name} value={this.props.value} className={this.props.className} onChange={this.props.onChange} id={this.props.id} />
+            </label>
+
+
+        </>
+
+    }
+}
+
+
+export default Checkbox
---
+++ src/app/components/RadioBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { throwStatement } from '@babel/types';
+
+
+class RadioBox extends React.Component {
+
+
+
+    render() {
+        return <>
+            <label for={this.props.for}>
+                {this.props.children} <input type='radio' name={this.props.name} value={this.props.value} onchange={this.props.onchange} id={this.props.id} />
+            </label>
+
+        </>
+    }
+}
